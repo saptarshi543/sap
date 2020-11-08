438d27dc6bc7a3c540fa3c9ff76d703bb7130e4c
Saptarshi Chattopadhyay <69950501+saptarshi543@users.noreply.github.com>
2020-11-08 09:15:55 +0530

    neon.java
    IT MAY BE EDITED IN THE FUTURE
    .................
    WORK UNDER PROGRESS

--- neon.java
+++ neon.java
@@ -3,20 +3,21 @@ public class neon
 {
 	public static void main(String sap[])
 	{
-		int num2,num3,len,i,next,yt1;
+		int num2,num3,len,i,yt1=0;
+		int next=0;
 		String num,hi;
 		String yt="";
 		Scanner nrt=new Scanner(System.in);
 		System.out.println("enter a number");
 		num=nrt.nextLine();
-		num2=Integer.parseInt(num)
+		num2=Integer.parseInt(num);
         num3=(num2*num2);
         hi=String.valueOf(num3);
         len=hi.length();
         for (i=0;i<len ;i++ ) {
         	char ch=hi.charAt(i);
         	yt=yt+ch;
-        	yt1=Integer.parseInt(yt)
+        	yt1=Integer.parseInt(yt);
             next=next+yt1;
         }
         if (yt1==num2) {
